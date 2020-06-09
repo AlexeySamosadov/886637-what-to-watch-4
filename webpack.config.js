a740489f675e1051ee43acba326bb8f598548447
AlexeySamosadov <asamosadov@gmail.com>
2020-06-09 14:37:03 +0300

    finished -lesson task2

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require(`path`);
+const projectLink = path.join(__dirname, `public`);
+
+module.exports = {
+  entry: `./src/index.js`,
+  output: {
+    filename: "bundle.js",
+    path: projectLink,
+  },
+  devServer: {
+    contentBase: projectLink,
+    open: true,
+    inline: true,
+    port: 1337,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`,
+        }
+      }
+    ]
+  },
+  devtool: `source-map`
+};
