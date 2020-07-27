765177a7fc9baf54a33f4268352e1ca948eb1184
AlexeySamosadov <asamosadov@gmail.com>
2020-07-27 21:26:08 +0300

    d

--- src/components/film-info/film-info.jsx
+++ src/components/film-info/film-info.jsx
@@ -126,7 +126,10 @@ const FilmInfo = ({activeFilm, films, updateFavouriteFilms}) => {
 
         <footer className="page-footer">
           <div className="logo">
-            <a href="main.html" className="logo__link logo__link--light">
+            <a href="#" onClick={(e)=>{
+              e.preventDefault();
+              history.push(AppRoute.MAIN);
+            }} className="logo__link logo__link--light">
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
               <span className="logo__letter logo__letter--3">W</span>
